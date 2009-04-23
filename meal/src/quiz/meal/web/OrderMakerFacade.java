8c09235b79777a2aa5ab8ceb6b776a214918a7f4
siuying <siu.ying@gmail.com>
2009-04-24 01:01:42 +0800

    disable CPS order maker bcoz it as access denied error from GAE

--- meal/src/quiz/meal/web/OrderMakerFacade.java
+++ meal/src/quiz/meal/web/OrderMakerFacade.java
@@ -1,39 +1,46 @@
 package quiz.meal.web;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import quiz.meal.Menu;
 import quiz.meal.OrderMaker;
 import quiz.meal.model.Item;
 
 public class OrderMakerFacade {
+    private Logger log = Logger.getLogger(OrderMakerFacade.class);
     private OrderMaker orderMaker;
     private Menu menu;
-    
-    public String[] makeOrder(String ... wantedFood) {
+
+    public String[] makeOrder(String... wantedFood) {
+        log.info("make order: " + Arrays.toString(wantedFood));
         List<Item> wantedItems = menu.getItems(wantedFood);
         List<Item> order = orderMaker.order(wantedItems.toArray(new Item[0]));
         return getItemNames(order.toArray(new Item[0]));
     }
 
-    private String[] getItemNames(Item ... items) {
+    private String[] getItemNames(Item... items) {
         List<String> orderItemNames = new ArrayList<String>();
-        for(Item i : items) {
+        for (Item i : items) {
             orderItemNames.add(i.getName());
         }
         return orderItemNames.toArray(new String[0]);
     }
 
     /**
-     * @param orderMaker the orderMaker to set
+     * @param orderMaker
+     *            the orderMaker to set
      */
     public void setOrderMaker(OrderMaker orderMaker) {
         this.orderMaker = orderMaker;
     }
 
     /**
-     * @param menu the menu to set
+     * @param menu
+     *            the menu to set
      */
     public void setMenu(Menu menu) {
         this.menu = menu;
